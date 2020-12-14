556479111e004b9bdc0f72e930ea2ec26e717f67
#PiKa <piyush67.sharma@gmail.com>
2020-12-14 16:01:21 +0530

    find

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,52 +1,55 @@
 const Router=require("express");
 const router=Router();
-const bcrypt=require("bcrypt");
+const bcrypt=require("bcryptjs");
 const round=10;
 
 const {user,login}=require("../sequelize");
-const { request1 } = require("express");
+// const { request1 } = require("express");
 
-router.post("/",async (request,res)=>{
-    const name=request.body.name;
-    const age=request.body.age;
-    const password=request.body.password;
-    const email=request.body.email;
+router.post("/",async (req,res)=>{
+    const name1=req.body.name;
+    const age1=req.body.age;
+    const password1=req.body.password;
+    const email1=req.body.email;
+    console.log("email",email1);
 
-    if (name==" "||age==" "||password==" "||email==" "){
+    if (name1===""|| age1===""|| password1===""|| email1===""){
         res.status(400).json({message:"please enter name,age,email,password"});
         return
     }
+
+    result=await user.findOne({
+        where:{
+            email:req.body.email,
+        },
+    });
+
+    result=result.dataValues;
+    console.log(result);
+    if (result.length===1){
+        res.status(400).json({
+            "message":"user already exists"
+        })
+    }
+
     let User=await user.create({
-        name:request.body.name,
-        age:request.body.age,
-        email:request.body.email,
+        name:req.body.name,
+        age:req.body.age,
+        email:req.body.email,
     });
     User=User.dataValues;
-   
-    console.log(password);
-    const pass= bcrypt.hash(password,10,(err,hash)=>{
-        if (err){
-            console.log("error is",err);
-        }
-        else{
-            login.hashedPassword=hash;
-            console.log("hashed",login.hashedPassword);
-        }
+  
+    const pass= bcrypt.hashSync(password1,10);
  
-    });
-    // console.log("pass",pass);
     let Login1=await login.create({
        
-        hashedPassword,
+        hashedPassword:pass,
         userId:User.id,
     });
     
-    console.log("User is",User);
-    res.status(200).json({data:Login1});
+    res.status(200).json({data:User});
     return
-    console.log("2");
-    res.end();
- 
+   
 });
 
 module.exports=router;
